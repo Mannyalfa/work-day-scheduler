38643efee41e18fdeb5550f2e9ed6e3af7f9f908
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-05-24 04:51:36 -0400

    fixed current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
  // Get current date
 
+/*const today = new Date();
+ *format.replace('mm', date.getMonth() + 1)
+.replace('yy', date.getFullYear());
+.replace('dd', date.getDate());
+document.getElementById("#currentday");*/
+
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
-});
+
 
