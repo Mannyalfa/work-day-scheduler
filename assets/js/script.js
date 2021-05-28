7d1d3a2a53d25099a91d09e43a0f7142683d56d8
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-05-28 02:58:47 -0400

    restructured much of the JavaScript, still a WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,93 @@
-
+//
 $(document).ready(function () {
-    $("#currentDay").text(moment().format("dddd MMMM Do, YYYY hh:mm A"));
-    $(".saveBtn").on("click", function () {
-    console.log(this);
-    var text = $(this).siblings(".description").val(); 
-    var time = $(this).parent().attr("id"); 
-    //set items in local storage.
-    localStorage.setItem(time, text);
-    })
-    //load data from LocalStorage.
-    $("#9hr .description").val(localStorage.getItem("9hr"));
-    $("#10hr .description").val(localStorage.getItem("10hr"));
-    $("#11hr .description").val(localStorage.getItem("11hr"));
-    $("#12hr .description").val(localStorage.getItem("12hr"));
-    $("#13hr .description").val(localStorage.getItem("13hr"));
-    $("#14hr .description").val(localStorage.getItem("14hr"));
-    $("#15hr .description").val(localStorage.getItem("15hr"));
-    $("#16hr .description").val(localStorage.getItem("16hr"));
-    $("#17hr .description").val(localStorage.getItem("17hr"));
+	var events = [];
 
-    function trackHours() {
-    //get current hour.
-    var currentHour = moment().hour();
+	$(".saveBtn").on("click", function () {
+		var text = $(this).siblings(".description").val();
+		var time = $(this).parent().attr("id");
+		var dateTime = moment().format("dddd, MMMM Do HH:mm");
+		events.push({ description: text, time: time, date: dateTime });
+		//set items in local storage.
+		localStorage.setItem("events", JSON.stringify(events));
+	});
 
-    // loop time slots
-    $(".time-slot").each(function () {
-    var slotHour = parseInt($(this).attr("id").split("hour")[1]);
-    console.log( slotHour, currentHour)
+	function timeCheck() {
+		//get current hour
+		var currentHour = moment().hour();
 
-    //check time slots
-    if (slotHour < currentHour) {
-    $(this).addClass("past");
-    $(this).removeClass("future");
-    $(this).removeClass("present");
-    }
-    else if (slotHour === currentHour) {
-    $(this).removeClass("past");
-    $(this).addClass("present");
-    $(this).removeClass("future");
-    }
-    else {
-    $(this).removeClass("present");
-    $(this).removeClass("past");
-    $(this).addClass("future");
-    }
-    })
-    }
-    trackHours(); //re-run function
+		//check time blocks
+		$(".time-block").each(function () {
+			var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+			if (blockHour < currentHour) {
+				$(this).addClass("past");
+			} else if (blockHour === currentHour) {
+				$(this).addClass("present");
+			} else {
+				$(this).addClass("future");
+			}
+		});
+	}
+	//recurring update time @ 60 second intervals
+	timeCheck();
+	var timeLeft = 60;
+	function getTime() {
+		setInterval(function () {
+			timeLeft--;
+
+			if (timeLeft === 0) {
+				timeCheck();
+				timeLeft = 60;
+			}
+		}, 1000);
+	}
+	getTime();
 });
+
+//reset for loop
+
+/*var currentDay = moment().format("dddd, MMMM Do");
+for(var i = 0; i < events.length; i++) {
+if(currentDay.isAfter(events[i].date)) {
+events[i].description = "";
+events[i].time = "";
+events[i].date = "";
+events.length = 0;
+}
+}
+ // load any saved data from localStorage
+var savedEvents = JSON.parse(localStorage.getItem("events"));
+
+if (savedEvents !== null) {
+events = savedEvents;
+}
+for(var i = 0; i < events.length; i++) {
+var savedDescription = events[i].description;
+$("#" + events[i].time).children(".description").text(savedDescription);
+}
+*/
 
 //Display current time in 12 hour format
 setInterval(function () {
-$("#currentDay").text(moment().format("dddd MMMM Do h:mm a"));
-}, 1000)
+	$("#currentDay").text(moment().format("dddd MMMM Do h:mm a"));
+}, 1000);
+
 //Display current time in 24 hour format
 setInterval(function () {
-$("#currentAlt").text(moment().format("dddd DD MMMM HH:mm:ss"));
-}, 1000)
+	$("#currentAlt").text(moment().format("dddd DD MMMM HH:mm:ss"));
+}, 1000);
 
 //Toggle 12/24 hour time mode
-$(function(){
-$('p.mil').hide();
-$('#currentAlt').hide();
-$('#toggle').on('click', function(){
-$('#currentAlt, #currentDay').toggle();
-$('p.ampm, p.mil').toggle();
-});
+$(function () {
+	$("p.mil").hide();
+	$("#currentAlt").hide();
+	$("#toggle").on("click", function () {
+		$("#currentAlt, #currentDay").toggle();
+		$("p.ampm, p.mil").toggle();
+	});
 });
 
 //Delete session storage and all user text
-$("#reset-buttton").on('click', function(){
-localStorage.clear();
+$("#reset-button").on("click", function () {
+	localStorage.clear();
 });
-    
-    
