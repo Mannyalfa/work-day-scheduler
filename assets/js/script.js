74581571595f0c93404e2a790cd399137d8658d5
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-05-29 17:50:51 -0400

    fixed time slot colors and clear-reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
 	var events = [];
-
+	//time block array
+	const timeBlockArr = [
+		$("#9"),
+		$("#10"),
+		$("#11"),
+		$("#12"),
+		$("#13"),
+		$("#14"),
+		$("#15"),
+		$("#16"),
+		$("#17"),
+	];
+	console.log(timeBlockArr);
+	//save button for event/appointment entries
 	$(".saveBtn").on("click", function () {
 		var text = $(this).siblings(".description").val();
 		var time = $(this).parent().attr("id");
@@ -8,32 +21,38 @@ $(document).ready(function () {
 		events.push({ description: text, time: time, date: dateTime });
 		//set items in local storage.
 		localStorage.setItem("events", JSON.stringify(events));
+		//empty field alert
 		if (text === "") {
 			alert("You have not made an entry. Please enter an event or appointment");
 		}
 	});
 	//compare current time to block hours
-	var currentTime = moment().hours();
-	var hour = $(this).parent().attr("id");
+
 	function timeCheck() {
 		//get current time using moment.js
-		if (hour < currentTime) {
-			$(this).addClass("past");
-		} else if (hour == currentTime) {
-			$(this).addClass("present");
-			$(this).removeClass("past");
-		} else {
-			$(this).addClass("future");
-			$(this).removeClass("present");
-			$(this).removeClass("past");
+		for (var i = 0; i < timeBlockArr.length; i++) {
+			var currentTime = moment().hours();
+			var hour = parseInt($(".time-block")[i].id);
+			//get array of elements and loop over array
+			var currentElem = $(".time-block")[i];
+			if (hour < currentTime) {
+				currentElem.classList.add("past");
+			} else if (hour === currentTime) {
+				currentElem.classList.add("present");
+				currentElem.classList.remove("past");
+			} else {
+				currentElem.classList.add("future");
+				currentElem.classList.remove("present");
+				currentElem.classList.remove("past");
+			}
 		}
 	}
+	//initiate time check for past/present/future events every 60 seconds
 	timeCheck();
 	var timeLeft = 60;
 	function getTime() {
 		setInterval(function () {
 			timeLeft--;
-
 			if (timeLeft === 0) {
 				timeCheck();
 				timeLeft = 60;
@@ -53,7 +72,7 @@ $(document).ready(function () {
 		$("#currentAlt").text(moment().format("dddd DD MMMM HH:mm:ss"));
 	}, 1000);
 
-	//Toggle 12/24 hour time mode
+	//toggle between 12 and 24 hour formats
 	$(function () {
 		$("p.mil").hide();
 		$("#currentAlt").hide();
@@ -63,11 +82,9 @@ $(document).ready(function () {
 		});
 	});
 
-	//Delete session storage and all user text
-	var form = $(this).closest("form");
+	//resets local storage and text boxes
 	$("#reset-button").on("click", function () {
-		$(".document").val("");
+		$(".description").val("");
 		localStorage.clear();
-
 	});
 });
