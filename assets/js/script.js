c20680ae28c7560617c502973800ed5e6bce1abc
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-05-30 14:48:05 -0400

    fixed loadStorage functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,43 @@
 $(document).ready(function () {
-    var events = [];
+	var events = [];
 	//pull events from local storage
-    function loadEvents() {
-		events = JSON.parse(localStorage.getItem(events));
-		console.log(events);
+	var loadStorage = localStorage.getItem("events");
+	var obj = JSON.parse(loadStorage);
+	//load events in textarea
+	function loadEvents() {
+		//for loop through all time blocks to load block time and text area
+		for (i = 9; i <= 17; i++) {
+			var tmp = i.toString();
+			//for loop for array of "event"
+			for (j = 0; j < obj.length; j++) {
+				//checking if event time is equal to current time
+				if (obj[j].time == tmp) {
+					var storeId = "#" + i
+					//append event to text area (fills in "description")
+					$(storeId).children("textarea").append(obj[j].description);
+				}
+			}
+		}
 	}
-	//populate text fields in HTML with events from local storage
+	//if loadstorage empty, load from storage
+	if (loadStorage) {
+		loadEvents();
+	}
+	//save button for event/appointment entries
+	$(".saveBtn").on("click", function () {
+		var text = $(this).siblings(".description").val();
+		var time = $(this).parent().attr("id");
+		/*var dateTime = moment().format("dddd, MMMM Do HH:mm");*/
+		events.push({ description: text, time: time });
+		//set items in local storage.
+		localStorage.setItem("events", JSON.stringify(events));
+		console.log(events);
+		//empty field alert
+	});
+	//compare current time to block hours
 
-
-    //time block array
-    const timeBlockArr = [
+	//time block array
+	const timeBlockArr = [
 		$("#9"),
 		$("#10"),
 		$("#11"),
@@ -20,19 +48,8 @@ $(document).ready(function () {
 		$("#16"),
 		$("#17"),
 	];
-    //save button for event/appointment entries
-	var text = $(this).siblings(".description").val();
-	var time = $(this).parent().attr("id");
-	var dateTime = moment().format("dddd, MMMM Do HH:mm");
-    $(".saveBtn").on("click", function () {
-		events.push({ description: text, time: time, date: dateTime });
-		//set items in local storage.
-		localStorage.setItem("events", JSON.stringify(events));
-		//empty field alert
-	});
-    //compare current time to block hours
 
-    function timeCheck() {
+	function timeCheck() {
 		//get current time using moment.js
 		for (var i = 0; i < timeBlockArr.length; i++) {
 			var currentTime = moment().hours();
@@ -51,10 +68,10 @@ $(document).ready(function () {
 			}
 		}
 	}
-    //initiate time check for past/present/future events every 60 seconds
-    timeCheck();
-    var timeLeft = 60;
-    function getTime() {
+	//initiate time check for past/present/future events every 60 seconds
+	timeCheck();
+	var timeLeft = 60;
+	function getTime() {
 		setInterval(function () {
 			timeLeft--;
 			if (timeLeft === 0) {
@@ -63,20 +80,20 @@ $(document).ready(function () {
 			}
 		}, 1000);
 	}
-    getTime();
+	getTime();
 
-    //Display current time in 12 hour format
-    setInterval(function () {
+	//Display current time in 12 hour format
+	setInterval(function () {
 		$("#currentDay").text(moment().format("dddd MMMM Do h:mm a"));
 	}, 1000);
 
-    //Display current time in 24 hour format
-    setInterval(function () {
+	//Display current time in 24 hour format
+	setInterval(function () {
 		$("#currentAlt").text(moment().format("dddd DD MMMM HH:mm:ss"));
 	}, 1000);
 
-    //toggle between 12 and 24 hour formats(includes seconds to check continuous update)
-    $(function () {
+	//toggle between 12 and 24 hour formats(includes seconds to check continuous update)
+	$(function () {
 		$("p.mil").hide();
 		$("#currentAlt").hide();
 		$("#toggle").on("click", function () {
@@ -85,10 +102,9 @@ $(document).ready(function () {
 		});
 	});
 
-    //resets local storage and text boxes
-    $("#reset-button").on("click", function () {
+	//resets local storage and text boxes
+	$("#reset-button").on("click", function () {
 		$(".description").val("");
 		localStorage.clear();
-
 	});
 });
